123e535f8be7768dd0d308edf1c987f4cd47fb68
MxSamol <msamolutchenko@gmail.com>
2021-12-24 15:32:49 +0700

    Third Commit

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ from settings import Settings
 from ship import Ship
 
 
-def run_game():
-    pygame.init()
-    settings = Settings()
-    screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
-    pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self.screen)
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(settings.bg_color)
-        ship.bltime()
-        pygame.display.flip()
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.screen.fill(self.settings.bg_color)
+            self.ship.bltime()
+            pygame.display.flip()
 
 
-run_game()
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
