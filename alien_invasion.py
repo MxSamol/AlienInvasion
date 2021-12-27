92f77817ce4e9bddb30efa1d18f3dae3c66aa920
MxSamol <msamolutchenko@gmail.com>
2021-12-27 13:27:14 +0700

    Commit #5

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,13 +3,14 @@ import pygame
 from settings import Settings
 from ship import Ship
 
-
 class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        icon = pygame.image.load('images/icon.bmp')
+        pygame.display.set_icon(icon)
         self.ship = Ship(self.screen)
 
     def run_game(self):
--- ship.py
+++ ship.py
@@ -1,21 +1,25 @@
 import pygame
+from settings import Settings
 
 
-class Ship:
-    def __init__(self, screen):
-        self.screen = screen
-        self.screen_rect = self.screen.get_rect()
+class Ship():
+    def __init__(self, ai_screen):
+        self.screen = ai_screen
+        self.settings = Settings()
+        self.screen_rect = ai_screen.get_rect()
         self.image = pygame.image.load('images/spaceship.bmp')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
         self.moving_right = False
         self.moving_left = False
+        self.x = float(self.rect.x)
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
 
     def bltime(self):
         self.screen.blit(self.image, self.rect)
